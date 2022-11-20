f10f78833bf1a1ab8216ea81d3e94765c0253bc6
Richard <richardstuart007@hotmail.com>
2022-11-20 20:00:29 +1300

    http://localhost:9103 client to whitelist

--- src/server/serverConstants.js
+++ src/server/serverConstants.js
@@ -50,5 +50,5 @@ exports.CORS_WHITELIST_LOCAL_REMOTE = [
 exports.CORS_WHITELIST_REMOTE = [
   'https://quizclient021render.onrender.com',
   'Postman',
-  'https://quizclient021n.netlify.app'
+  'http://localhost:9103'
 ]
